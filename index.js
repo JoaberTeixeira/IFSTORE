37ce7cd605ffb9dbb8475a6cf4a316656491f603
JoaberTeixeira <joaberdasilva81@gmail.com>
2024-09-02 17:31:56 -0300

    correcoes index

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 const app = express();
-const port = 3000;
+const PORT = 3000 
 
-app.set('view engine', 'ejs');
-app.set(express.urlencoded({extended: true}),)
+app.set ('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
 
-//criar usuario
-app.get('/cadastro', function (req, res)=> {
+app.get('/cadastro',(req,res)=>{
     res.render('cadastro')
-});
-app.listen(port);
+})
+
+
+app.listen(PORT);
