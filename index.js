d5451b682f4953040446afe4abd95bb67c14b398
JoaberTeixeira <joaberdasilva81@gmail.com>
2024-09-10 14:54:31 -0300

    introducao botstrap

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
 import express from 'express';
 const app = express();
-const PORT = 3000 
+const port = 3000;
 
-app.set ('view engine', 'ejs');
-app.use(express.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
-import usuario from './models/usuario.js';
+import usuario from './models/usuario.js'
 
-app.get('/cadastro',(req,res)=>{
+app.get('/cadastro', (req, res)=>{
     res.render('cadastro')
 })
 
-app.post('/cadastro', async (req, res) => {
-    /* const nome = req.body.nome;
-     const email = req.body.email;
-     const senha = req.body.senha;
-     const endereco = req.body.endereco;
-     const foto = req.body.foto;
-     const telefone = req.body.telefone;
-     const cpf = req.body.cpf;
-     const admin = req.body.admin;
-  */
-     const admin = req.body.admin == "on" ? true : false; 
-     const novoUsuario = new usuario({
-          nome : req.body.nome,
-          email : req.body.email,
-          senha : req.body.senha,
-          foto : req.body.foto,
-          telefone : req.body.telefone,
-          cpf : req.body.cpf,
-          admin : admin ,
-     });
-  
-     await novoUsuario.save();
-     res.send("Cadastrado com sucesso!")
-  
-  })
-//const newUsuario = new usuario(req.body)
+app.post('/cadastro', async (req, res)=>{
+    //esse comando equivale a um if
+    const admin = req.body.admin=="on" ? true : false;
 
-app.listen(PORT);
+    const novousuario = new usuario({
+        nome: req.body.nome,
+        email: req.body.email,
+        senha: req.body.senha,
+        endereco: req.body.endereco,
+        foto: req.body.foto,
+        telefone: req.body.telefone,
+        cpf: req.body.cpf,
+        admin: admin
+    })
+
+    await novousuario.save();
+    res.send("Cadastrado com sucesso!")
+})
+
+app.get('/login', (req, res)=>{
+    res.render('login')
+})
+
+app.post('/login', (req, res)=>{
+    res.redirect('/admin/usuarios/lst')})
+    app.get('/admin/usuarios/lst',(req,res)=>{
+        res.render('admin/usuarios/lst')
+     })
+  
+
+app.listen(port);
